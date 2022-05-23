1c61fe768f5e9a8fae4f3de6e208518427e21f18
sachimp <sachingandhi@live.com>
2022-05-24 00:30:56 +0100

    added a commented out resources routes for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :tasks
   get 'tasks', to: 'tasks#index'
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
